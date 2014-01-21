f7be9a3a628c2e1a0f85a74d7a9d6624ba3366da
Matthew Landauer <matthew@openaustralia.org>
2014-01-21 16:00:25 +1100

    Add icon_reset_xml library as submodule

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,5 @@
 require 'scraperwiki'
-require 'scrapers/icon_rest_xml'
+require './icon_rest_xml/scraper'
 
 scrape_icon_rest_xml("http://aps.gtcc.nsw.gov.au/ePlanning/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=290&o=xml")
 
