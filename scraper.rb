85a709d38e80abf810637e679cdf90cfdaa945a7
Matthew Landauer <matthew@openaustralia.org>
2014-01-21 16:11:36 +1100

    Make require work irrespective of current working directory

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,5 @@
 require 'scraperwiki'
-require './icon_rest_xml/scraper'
+require File.dirname(__FILE__) + '/icon_rest_xml/scraper'
 
 scrape_icon_rest_xml("http://aps.gtcc.nsw.gov.au/ePlanning/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=290&o=xml")
 
